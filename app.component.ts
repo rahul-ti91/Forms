d6ec4c4adf395c2256a0177cd06c561001bb566a
rahul-ti91 <rahul.ti91@gmail.com>
2016-06-27 11:26:43 +0530

    latest changes
    Latest changes

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { HeroFormComponent } from "./hero-form.component";
+
+@Component({
+	selector:"my-app",
+	template: `<hero-form></hero-form>`,
+	directives: [HeroFormComponent]
+})
+
+export class AppComponent{
+	title:string = "Details of the Hero"; 
+}
